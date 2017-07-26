9eb9b29f5626d1c8adc36e210ba982fe6fa5dc4d
Egor Zyuskin <ezyuskin@amaxlab.ru>
2017-07-26 21:46:54 +0600

    Update copyright

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the yandex-pdd-api project.
+ *
+ * (c) AmaxLab 2017 <http://www.amaxlab.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AmaxLab\YandexPddApi\Tests;
 
 use AmaxLab\YandexPddApi\Manager\DomainManager;
